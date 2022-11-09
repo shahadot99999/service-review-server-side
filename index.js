9c1b49b134f9e4f96c70881380a3cec074ee5b8b
shahadot99999 <shahadot99999@gmail.com>
2022-11-09 22:18:46 +0600

    appointment and service set up mongo db

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -13,11 +13,41 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@mangoodbfirstproject.65jgjko.mongodb.net/test`;
-// console.log(uri);
-// MongoClient.connect(uri, function (err, client) {
-//     const collection = client.db("test").collection("devices");
-// })
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        const serviceCollection = client.db('doctorDb').collection('services');
+        const appointmentCollection = client.db('doctorDb').collection('appointments');
+
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+            console.log(services);
+        })
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        })
+
+        //appointment
+        app.post('/appointments', async (req, res) => {
+            const appointment = req.body;
+            const result = await appointmentCollection.insertOne(appointment);
+            res.send(result);
+        })
+    }
+    finally {
+
+    }
+}
+
+run()
 app.get('/', (req, res) => {
     res.send('KLA Doctor server is running');
 })
